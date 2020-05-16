07dcd8045410e80a8700242c5c0d7a5d12395994
Melroy van den Berg <melroy@melroy.org>
2020-05-16 19:57:22 +0200

    Clean-up

--- run.sh
+++ run.sh
@@ -12,6 +12,7 @@ SPICE_PASSWD=${SPICE_PASSWD:-"password"}
 SPICE_KB_LAYOUT=${SPICE_KB_LAYOUT:-"us"}
 SPICE_KB_VARIANT=${SPICE_KB_VARIANT:-"euro"}
 SPICE_SOUND=${SPICE_SOUND:-false}
+SPICE_PASS_NEEDED=${SPICE_PASS_NEEDED:-"false"}
 PASS_NEEDED=${PASS_NEEDED:-"false"}
 SUDO=${SUDO:-false}
 
@@ -23,17 +24,15 @@ echo $TIMEZONE > /etc/timezone
 useradd -ms /bin/bash -u $SPICE_UID $SPICE_USER
 echo "$SPICE_USER:$SPICE_PASSWD" | chpasswd
 # Add new user to several groups
-if [ "$SUDO" = false ] ; then
+if [ "$SUDO" = false ]; then
   usermod -a -G sudo,adm,audio,video,plugdev $SPICE_USER
 fi
-if [ "$PASS_NEEDED" = false ] ; then
+if [ "$PASS_NEEDED" = false ]; then
   # No password needed
   echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 fi
 
 # Adapt several files
-sed -i "s|#Option \"SpicePassword\" \"\"|Option \"SpicePassword\" \"$SPICE_PASSWD\"|" /etc/X11/spiceqxl.xorg.conf
-unset SPICE_PASSWD
 update-locale LANG=$SPICE_LOCAL
 sed -i "s/XKBLAYOUT=.*/XKBLAYOUT=\"$SPICE_KB_LAYOUT\"/" /etc/default/keyboard
 sed -i "s/XKBVARIANT=.*/XKBVARIANT=\"$SPICE_KB_VARIANT\"/" /etc/default/keyboard
@@ -45,7 +44,9 @@ sed -i "s/SPICE_SOUND/$SPICE_SOUND/" /etc/xdg/autostart/xfcesettings.desktop
 sed -i "s/SPICE_USER/$SPICE_USER/" /etc/xdg/autostart/sound.desktop
 
 # Enable PulseAudio
-if [ "$SPICE_SOUND" = true ] ; then
+PARAM_SOUND=""
+if [ "$SPICE_SOUND" = true ]; then
+  PARAM_SOUND="--audio-fifo-dir=/tmp/audio_fifo"
   mkdir /tmp/audio_fifo
   chown $SPICE_USER.$SPICE_USER /tmp/audio_fifo
   FIFO=/tmp/audio_fifo/audio.fifo
@@ -71,11 +72,14 @@ cd /home/$SPICE_USER
 echo "Set disable_coredump false" >> /etc/sudo.conf
 
 # Start both X server with Spice Server (don't ask for login)
-if [ "$SPICE_SOUND" = true ] ; then
-  Xspice --port 5900 --audio-fifo-dir=/tmp/audio_fifo --auto --vdagent --disable-ticketing $DISPLAY > /dev/null 2>&1 &
-else
-  Xspice --port 5900 --auto --vdagent --disable-ticketing $DISPLAY > /dev/null 2>&1 &
+PARAM_PASSWORD="--disable-ticketing"
+if [ "$SPICE_PASS_NEEDED" = true ]; then
+  PARAM_PASSWORD="--password $SPICE_PASSWD"
 fi
+unset SPICE_PASSWD
+
+# Start Spice server with Xorg server!
+Xspice --port 5900 $PARAM_SOUND --auto --vdagent $PARAM_PASSWORD $DISPLAY > /dev/null 2>&1 &
 sleep 1
 
 # Enable WebSockify for SPICE HTML5 client
@@ -86,7 +90,6 @@ mkdir -p /run/user/$SPICE_UID
 chown $SPICE_USER.$SPICE_USER /run/user/$SPICE_UID
 
 # Export some env variables
-export HOME=/home/$SPICE_USER
 export DESKTOP_SESSION=xfce
 export XDG_SESSION_TYPE=x11
 export XDG_DATA_DIRS=/usr/share/xfce4:/usr/local/share:/usr/share
@@ -94,7 +97,6 @@ export XDG_SESSION_DESKTOP=xfce
 export XDG_CURRENT_DESKTOP=XFCE
 export XDG_CONFIG_DIRS=/etc/xdg/xdg-xfce:/etc/xdg
 export XDG_RUNTIME_DIR=/run/user/$SPICE_UID
-
 
 # Start DBUS session with XFCE4 session
 # TODO: Use supervisor
